14a67189ca05f109a1120bf1083805fd42ac0924
Ameya Srivastava <0ameya.sr@gmail.com>
2024-08-10 00:41:04 +0530

    Added and prepared data for the diet recommendation task

--- Model/dataset_builder.py
+++ Model/dataset_builder.py
@@ -17,9 +17,33 @@ def make_features():
     features = pandas.concat([PRICE, AVG_RATING, NUM_RATING, PACKAGING, CFOOT, RECYCLE, BIODEG, ORGANIC, EMISSIONS, IMPORTED, PROCESSED], axis=1)
     return features
 
-def commit_dataset(features,path="model/ec_food_dataset.csv"):
+def make_diet_features():
+    ENERGY = pandas.DataFrame([random.randint(0,2) for _ in range(500)],columns=["ENERGY"])
+    TOTFAT = pandas.DataFrame([random.randint(0,2) for _ in range(500)],columns=["TOTFAT"])
+    CHOL = pandas.DataFrame([random.randint(0,2) for _ in range(500)],columns=["CHOL"])
+    TOTALCARB = pandas.DataFrame([random.randint(0,2) for _ in range(500)],columns=["TOTALCARB"])
+    ADDSUG = pandas.DataFrame([random.randint(0,2) for _ in range(500)],columns=["ADDSUG"])
+    PROT = pandas.DataFrame([random.randint(0,2) for _ in range(500)],columns=["PROT"])
+    VITD = pandas.DataFrame([random.randint(0,2) for _ in range(500)],columns=["VITD"])
+    VITA = pandas.DataFrame([random.randint(0,2) for _ in range(500)],columns=["VITA"])
+    VITB2 = pandas.DataFrame([random.randint(0,2) for _ in range(500)],columns=["VITB2"])
+    VITB12 = pandas.DataFrame([random.randint(0,2) for _ in range(500)],columns=["VITB12"])
+    VITC = pandas.DataFrame([random.randint(0,2) for _ in range(500)],columns=["VITC"])
+    CALC = pandas.DataFrame([random.randint(0,2) for _ in range(500)],columns=["CALC"])
+    SOD = pandas.DataFrame([random.randint(0,2) for _ in range(500)],columns=["SOD"])
+    IRON = pandas.DataFrame([random.randint(0,2) for _ in range(500)],columns=["IRON"])
+    POT = pandas.DataFrame([random.randint(0,2) for _ in range(500)],columns=["POT"])
+    PHOS = pandas.DataFrame([random.randint(0,2) for _ in range(500)],columns=["PHOS"])
+    
+    features = pandas.concat([ENERGY,TOTFAT,CHOL,TOTALCARB,ADDSUG,PROT,VITD,VITA,VITB2,VITB12,VITC,CALC,SOD,IRON,POT,PHOS], axis=1)
+    return features
+
+def commit_dataset(features,path="Model/data/ec_diet_dataset.csv"):
     try:
         features.to_csv(path)
         print(f"Dataset successfully commited to {path}")
     except Exception as e:
         print(f"commit_dataset() > An error was encountered: {e}")
+
+if __name__ == "__main__":
+    commit_dataset(make_diet_features())
